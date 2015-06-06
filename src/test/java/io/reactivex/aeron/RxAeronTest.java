bdbfd4b90496bfa7958dd4f0f6722e6d03f8a4d4
Robert Roeser <rroeser@netflix.com>
2015-06-05 17:27:01 -0700

    testing sending 10k messages

--- src/test/java/io/reactivex/aeron/RxAeronTest.java
+++ src/test/java/io/reactivex/aeron/RxAeronTest.java
@@ -13,7 +13,7 @@ public class RxAeronTest extends TestCase {
     public static final String CHANNEL = "aeron:udp?remote=localhost:43450";
 
     @Test
-    public void testUnicastServerAndClient() throws Exception {
+    public void testUnicastServerAndClientForTen() throws Exception {
         RxAeron instance = RxAeron.getInstance();
 
 
@@ -23,7 +23,7 @@ public class RxAeronTest extends TestCase {
         instance.createUnicastServer(CHANNEL, (buffer) ->
            buffer.map(b -> {
                String s = new String(b.byteArray());
-               System.out.println("handling => " + s);
+               System.out.println(Thread.currentThread() + " -- handling => " + s);
 
                countDownLatch.countDown();
 
@@ -40,6 +40,36 @@ public class RxAeronTest extends TestCase {
         unicastClient
                 .offer(buffer)
                 .toBlocking().last();
+
+        countDownLatch.await();
+    }
+
+    @Test
+    public void testUnicastServerAndClientForTenThousand() throws Exception {
+        RxAeron instance = RxAeron.getInstance();
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(10_000);
+
+        UnicastClient unicastClient = instance.createUnicastClient(CHANNEL);
+        instance.createUnicastServer(CHANNEL, (buffer) ->
+                buffer.map(b -> {
+
+                    countDownLatch.countDown();
+
+                    return null;
+                })
+        );
+
+
+        Observable<DirectBuffer> buffer = Observable
+            .range(1, 10_000)
+            .map(i -> "sending_" + i)
+            .map(s -> new UnsafeBuffer(s.getBytes()));
+
+        unicastClient
+            .offer(buffer)
+            .toBlocking().last();
 
         countDownLatch.await();
     }
